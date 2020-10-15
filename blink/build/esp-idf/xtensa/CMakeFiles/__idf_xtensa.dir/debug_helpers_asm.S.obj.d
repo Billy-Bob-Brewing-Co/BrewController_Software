c23160680f7885f917b20532c3bacdd9ebc4b523
William Rooke <william.rooke1@hotmail.com>
2020-10-15 20:58:30 +1100

    Blinky beginnings

---
+++ blink/build/esp-idf/xtensa/CMakeFiles/__idf_xtensa.dir/debug_helpers_asm.S.obj.d
@@ -0,0 +1,13 @@
+esp-idf/xtensa/CMakeFiles/__idf_xtensa.dir/debug_helpers_asm.S.obj: \
+ F:/esp-idf/components/xtensa/debug_helpers_asm.S \
+ F:/esp-idf/components/xtensa/include/xtensa/coreasm.h \
+ F:/esp-idf/components/xtensa/esp32/include/xtensa/config/core.h \
+ F:/esp-idf/components/xtensa/include/xtensa/hal.h \
+ F:/esp-idf/components/xtensa/include/xtensa/xtensa-versions.h \
+ F:/esp-idf/components/xtensa/esp32/include/xtensa/config/core-isa.h \
+ F:/esp-idf/components/xtensa/esp32/include/xtensa/config/core-matmap.h \
+ F:/esp-idf/components/xtensa/esp32/include/xtensa/config/tie.h \
+ F:/esp-idf/components/xtensa/esp32/include/xtensa/config/tie-asm.h \
+ F:/esp-idf/components/xtensa/esp32/include/xtensa/config/specreg.h \
+ F:/esp-idf/components/xtensa/include/xtensa/corebits.h \
+ F:/esp-idf/components/xtensa/esp32/include/xtensa/config/system.h
---
+++ blink/build/esp-idf/xtensa/CMakeFiles/__idf_xtensa.dir/eri.c.obj.d
@@ -0,0 +1,10 @@
+esp-idf/xtensa/CMakeFiles/__idf_xtensa.dir/eri.c.obj: \
+ F:/esp-idf/components/xtensa/eri.c \
+ f:\.espressif\tools\xtensa-esp32-elf\esp-2019r2-8.2.0\xtensa-esp32-elf\lib\gcc\xtensa-esp32-elf\8.2.0\include\stdint.h \
+ f:\.espressif\tools\xtensa-esp32-elf\esp-2019r2-8.2.0\xtensa-esp32-elf\xtensa-esp32-elf\sys-include\stdint.h \
+ f:\.espressif\tools\xtensa-esp32-elf\esp-2019r2-8.2.0\xtensa-esp32-elf\xtensa-esp32-elf\sys-include\machine\_default_types.h \
+ f:\.espressif\tools\xtensa-esp32-elf\esp-2019r2-8.2.0\xtensa-esp32-elf\xtensa-esp32-elf\sys-include\sys\features.h \
+ f:\.espressif\tools\xtensa-esp32-elf\esp-2019r2-8.2.0\xtensa-esp32-elf\xtensa-esp32-elf\sys-include\_newlib_version.h \
+ f:\.espressif\tools\xtensa-esp32-elf\esp-2019r2-8.2.0\xtensa-esp32-elf\xtensa-esp32-elf\sys-include\sys\_intsup.h \
+ f:\.espressif\tools\xtensa-esp32-elf\esp-2019r2-8.2.0\xtensa-esp32-elf\xtensa-esp32-elf\sys-include\sys\_stdint.h \
+ F:/esp-idf/components/xtensa/include/eri.h

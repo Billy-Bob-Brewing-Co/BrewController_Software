046e49aa7c2101ab19349d124b5590d762c904d9
Rob Carey <rob_carey_1@hotmail.com>
2020-11-10 10:24:37 +1100

    Added Http module
    Problem
    - Need a way to validate credentials and register device
    
    Solution
    - Use http requests

---
+++ main/config.h
@@ -0,0 +1,26 @@
+/* DESCRIPTION ***************************************************
+
+ File:                config.h
+
+ Author:              Robert Carey
+
+ Creation Date:       9th November 2020
+
+ Description:         
+
+ END DESCRIPTION ***************************************************************/
+
+#ifndef MAIN_CONFIG_H_
+#define MAIN_CONFIG_H_
+
+#define NVS_PART_KEY "storage"         // partition key
+#define NVS_KEY_WIFI_SSID "wifissid"   //string
+#define NVS_KEY_WIFI_PASS "wifipass"   //string
+#define NVS_KEY_USER_ID "userid"       //string
+#define NVS_KEY_DEVICE_ID "deviceid"   //string
+#define NVS_KEY_USER_EMAIL "useremail" //string
+#define NVS_KEY_USER_PASS "userpass"   //string
+#define NVS_KEY_USER_JWT "userjwt"     //string
+#define NVS_KEY_RST_CNT "rstcnt"       //uint16
+
+#endif

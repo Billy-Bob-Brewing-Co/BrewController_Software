5d0073816e253ee5a022f07773b21d0a5ccf7267
Rob Carey <rob_carey_1@hotmail.com>
2020-11-09 22:07:15 +1100

    Added comms module framework
    Problem
    - Need a place to configure and init the communications
    
    Solutin
    - Created comms module

---
+++ main/comms.c
@@ -0,0 +1,21 @@
+/* DESCRIPTION ***************************************************
+
+ File:                comms.c
+
+ Author:              Robert Carey
+
+ Creation Date:       9th November 2020
+
+ Description:         
+
+ END DESCRIPTION ***************************************************************/
+
+#include "esp_log.h"
+
+#include "comms.h"
+
+static const char *TAG = "COMMS";
+
+void comms_init(const TCommsSetup *const commsSetup)
+{
+}

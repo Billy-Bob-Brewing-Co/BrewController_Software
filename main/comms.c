9308a2f15d0e680dd2dbbf27c6afa12155af91c5
Rob Carey <rob_carey_1@hotmail.com>
2020-11-09 23:04:02 +1100

    Added wifi init module
    Problem
    - Needed wifi init module
    
    Solution
    - Added wifi module from example provided by espressif
    
    Note
    - Hard coded access point credentials

--- main/comms.c
+++ main/comms.c
@@ -10,12 +10,21 @@
 
  END DESCRIPTION ***************************************************************/
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 #include "esp_log.h"
+#include "freertos/semphr.h"
 
+#include "wifi.h"
 #include "comms.h"
 
 static const char *TAG = "COMMS";
 
 void comms_init(const TCommsSetup *const commsSetup)
 {
+  ESP_LOGI(TAG, "ESP_WIFI_MODE_STA");
+  wifi_init_sta(commsSetup->wifiSsid, commsSetup->wifiPass);
 }
